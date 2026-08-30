2c91b4e5bfee98a8fe2b17468c3d1bdfe0fd8a5c
Anuj Raghav <anuj.ehep@gmail.com>
2026-08-30 19:30:34 +0200

    Condor submission file

--- BDT_XGBoost/run_bdt.sh
+++ BDT_XGBoost/run_bdt.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-cd /afs/cern.ch/user/a/araghav/Analyses/Run3/ml-hep/BDT
-python3 ClassificationBDTHgg.py
+cd /afs/cern.ch/user/a/araghav/Analyses/Run3/ml-hep/BDT_XGBoost
+python3 xgb_engine.py
